4937ea25d770ba33acb82c71307ceb2011c4ee7a
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2024-09-22 00:21:51 +0530

    main layout for studio

---
+++ app/(studio)/layout.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'Backend | Photon eye center',
+    description: '',
+  }
+  
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+  
